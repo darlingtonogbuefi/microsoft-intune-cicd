a7a3be124c12a9fd54e47ed99d39ff41006fc99d
Darlington Ogbuefi <darlingtonogbuefi@yahoo.com>
2025-07-15 21:57:48 +0100

    1

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage"
+    storage_account_name = "tfstatestorage946"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
